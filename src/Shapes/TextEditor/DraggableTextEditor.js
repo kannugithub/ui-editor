eba24e7e7c3866212fcdc91ada63cc25d7acb839
Karan Singharia <karangni123@gmail.com>
2023-12-14 16:25:01 +0530

    new text changes

--- src/Shapes/TextEditor/DraggableTextEditor.js
+++ src/Shapes/TextEditor/DraggableTextEditor.js
@@ -3,27 +3,34 @@ import Draggable from "react-draggable";
 
 const DraggableTextEditor = () => {
   const [text, setText] = useState("");
+  const [savedTexts, setSavedTexts] = useState([]);
 
   const handleTextChange = (e) => {
     setText(e.target.value);
   };
 
+  const handleSave = () => {
+    setSavedTexts([...savedTexts, text]);
+    setText(""); // Clear the input after saving
+  };
+
   return (
-    <Draggable>
-      <div
-        style={{width: "300px", position: "relative", zIndex: "1" }}
-      >
-        <input
-          value={text}
-          onChange={handleTextChange}
-          placeholder="Type your text here..."
-          style={{ width: "100%", resize: "none" }}
-        />
-        <div>
-          <pre style={{color: "white", fontSize: "14px"}}>{text}</pre>
-        </div>
+    <div style={{ width: "300px", position: "relative", zIndex: "1" }}>
+      <input
+        value={text}
+        onChange={handleTextChange}
+        placeholder="Type your text here..."
+        style={{ width: "100%", resize: "none" }}
+      />
+      <div>
+        {savedTexts.map((savedText, index) => (
+          <Draggable key={index}>
+            <pre style={{ color: "white", fontSize: "14px" }}>{savedText}</pre>
+          </Draggable>
+        ))}
       </div>
-    </Draggable>
+      <button onClick={handleSave}>Save</button>
+    </div>
   );
 };
 
